bd0d2ac9c7b76400278e64521d6c7405551fc455
mohammad aliani <m.aliani.wiki@gmail.com>
2025-05-22 11:28:06 +0330

    use from class ReadFile for read a text file

--- src/io/ReadTextFile.java
+++ src/io/ReadTextFile.java
@@ -9,10 +9,13 @@ public class ReadTextFile {
     public static void main(String[] args) {
         try {
             // FileReader is a subclass of Reader . use for read text file .
+            // with create object file open for read .
             FileReader fileReader = new FileReader("/home/mohammad/IdeaProjects/base/resources/mohammad.txt");
             int chCode;
+            // usually not use from read() method for read text file char by char
             while (-1 != (chCode=fileReader.read())) // read() method return int because for understand file read is finished return -1
                 System.out.println("Next:"+(char) chCode);
+            // close file is important for free memory
             fileReader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
