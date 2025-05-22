11e7f07ca46754f65250a18070a93b0a3a8837dd
mohammad aliani <m.aliani.wiki@gmail.com>
2025-05-22 15:02:37 +0330

    class file help to work with files

--- src/io/FileHelpToWorkWithFile.java
+++ src/io/FileHelpToWorkWithFile.java
@@ -5,6 +5,9 @@ import java.io.File;
 public class FileHelpToWorkWithFile {
     public static void main(String[] args) {
         // help to work with files , is not for read or write file
+        // character / is for separate folder in windows should use \
+        // in windows is important if use \t or \n in file path , \ character is escape character in java and tab or new line if used
+        // for solve this problem use / or use \\ to solve this problem.
         File f = new File("/home/mohammad/IdeaProjects/base/resources/file.docx");
         long length = f.length();
         boolean isdir = f.isDirectory();
